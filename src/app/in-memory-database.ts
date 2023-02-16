5cd0288f1b88dd4b496013b86ad3c82fb0b3ad19
alefmeira <alef.chaves01@gmail.com>
2023-02-16 09:26:28 -0300

    adding in-memory-server and model for category

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,18 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.model";
+
+export class InMemoryDatabase implements InMemoryDbService {
+
+    createDb() {
+    const categories: Category[] = [
+        { id: 1, name: "Moradia", description: "Pagamentos de contas da casa"},
+        { id: 2, name: "Saúde", description: "Plano de saúde e remédios"},
+        { id: 3, name: "Lazer", description: "Cinema, paraia, restaurante, teatro"},
+        { id: 4, name: "Salário", description: "Recebimento de salário"},
+        { id: 5, name: "Freelas", description: "Trabalho como freelancer"},
+    ];
+
+    return { categories }
+    }
+
+}
