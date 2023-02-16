4eb3796dcbf3a9d35669c6c4cacdb5cc4daccd27
alefmeira <alef.chaves01@gmail.com>
2023-02-16 10:48:18 -0300

    adding category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, elementAt, map, Observable, throwError } from 'rxjs';
+import { Category } from './category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath: string = "api/categories";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<Category[]> {
+    return this.httpClient.get(this.apiPath).pipe(
+      catchError(this.handlerError),
+      map(this.jsonDataToCategories)
+    );
+  }
+
+  getById(id: number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.httpClient.get(url).pipe(
+      catchError(this.handlerError),
+      map(this.jsonDataToCategory)
+    );
+  }
+
+  create(category: Category): Observable<Category> {
+    return this.httpClient.post(this.apiPath, category).pipe(
+      catchError(this.handlerError),
+      map(this.jsonDataToCategory)
+    );
+  }
+
+  update(category: Category): Observable<Category> {
+    const url = `${this.apiPath}/${category.id}`;
+  
+    return this.httpClient.put(url, category).pipe(
+      catchError(this.handlerError),
+      map(() => category)
+    );
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.httpClient.delete(url).pipe(
+      catchError(this.handlerError),
+      map(() => null)
+    )
+  }
+
+  private jsonDataToCategory(jsonData: any) {
+    return jsonData as Category;
+  }
+
+  private jsonDataToCategories(jsonData: any[]) {
+    const categories: Category[] = [];
+    jsonData.forEach(element => categories.push(jsonData as Category));
+    return categories;
+  }
+
+  private handlerError(error: any): Observable<any> {
+    console.log("Error na requisição => ", error);
+    return throwError(() => error);
+  }
+
+}
